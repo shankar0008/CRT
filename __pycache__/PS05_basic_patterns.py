0831aed63eee0e18d27be52d1a1e64c0aceb16ba
shankar0008 <avulasaishankar@gmail.com>
2026-02-11 16:15:46 +0530

    sai

--- __pycache__/PS05_basic_patterns.py
+++ __pycache__/PS05_basic_patterns.py
@@ -13,11 +13,11 @@ for i in range(n):
         print("*", end=" ")
     print()
 '''
-'''
-n = int(input())
-for i in range(n):
-    for j in range(n-i):
-        print("*", end=" ")
-    print()
+
+'''n = int(input())
+for i in range(1, n+1):     
+    for j in range(i):
+        print(j+1, end=" ")   
+    print() 
 '''
 

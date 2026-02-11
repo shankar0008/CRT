a718a22ce64c3c6d8a9277e61895e45054d26ae1
shankar0008 <avulasaishankar@gmail.com>
2026-02-11 15:58:00 +0530

    mess

---
+++ __pycache__/PS05_basic_patterns.py
@@ -0,0 +1,23 @@
+'''
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+'''
+'''
+n = int(input())
+for i in range(n):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
+'''
+'''
+n = int(input())
+for i in range(n):
+    for j in range(n-i):
+        print("*", end=" ")
+    print()
+'''
+
